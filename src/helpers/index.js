75c93c27d1f78e988985441d4f13f6c2bd41cf13
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-12-05 12:45:42 +0400

    Code clean up

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,20 @@
 import ru from "date-fns/locale/ru";
 import { format, parseISO } from "date-fns";
+import { EVENT_MODE, EVENT_TYPES, ADDITIONAL_INPUTS } from "./../constants";
 
 export const formatDate = (date) => {
   return format(parseISO(date), "d MMMM yyyy", { locale: ru });
+};
+
+export const getInitialState = (state) => {
+  return state && state.mode === EVENT_MODE.EDIT
+    ? {
+        ...state.item,
+        type: EVENT_TYPES.find((e) => e.key === state.item.type),
+      }
+    : {
+        eventName: "",
+        type: EVENT_TYPES[0],
+        additionalFields: ADDITIONAL_INPUTS[EVENT_TYPES[0].key],
+      };
 };
